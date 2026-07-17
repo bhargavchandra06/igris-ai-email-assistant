581f9291dfc8d78f89cbfa9f3ba2c7484555078f
G.Bhargav Chandra <g.bhargavchandra@GBhargavs-MacBook-Air.local>
2026-07-18 01:14:11 +0530

    Test ec2 deploy

--- igiris-backend/src/main/java/com/email/writer/config/CorsProperties.java
+++ igiris-backend/src/main/java/com/email/writer/config/CorsProperties.java
@@ -3,7 +3,7 @@ package com.email.writer.config;
 import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "app.cors")
+@ConfigurationProperties(prefix = "app.cors")// adding automate deploy to ec2
 public record CorsProperties(List<String> allowedOrigins, List<String> allowedOriginPatterns) {
 
 	public CorsProperties {
